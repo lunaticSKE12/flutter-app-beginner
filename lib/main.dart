db1763661bdf320cc713f7bc542e607f9a68828a
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-01 15:01:38 +0700

    Add package url_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,9 +16,24 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  final String _url = 'https://flutter.dev';
+
+  void _launchURL() async {
+    if (!await launch(_url)) throw 'Could not launch $_url';
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _launchURL,
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text('Webview'),
+      ),
+      body: Container(),
+    );
   }
 }
