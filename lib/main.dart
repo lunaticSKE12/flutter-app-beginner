31bcbf55faf04bd30ffcdc5b3cf472f03499443f
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-01 13:54:15 +0700

    Image asset/ network border radius

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,32 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child:
+              // Image.asset(
+              //   'assets/images/260282250_331057381690171_8728174700271994000_n.jpg',
+              ClipRRect(
+            borderRadius: BorderRadius.circular(25),
+            child: Image.network(
+              'https://images.pexels.com/photos/10800242/pexels-photo-10800242.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+              width: 350,
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+        // CircleAvatar(
+        //     radius: 250,
+        //     backgroundImage: NetworkImage(
+        //         'https://images.pexels.com/photos/10800242/pexels-photo-10800242.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')
+        //     // AssetImage(
+        //     //     'assets/images/260282250_331057381690171_8728174700271994000_n.jpg'),
+
+        //     )
+      ],
+    ));
   }
 }
