1e849a3a96d0a68048199df3b2eaedb946b7053b
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-02 19:52:31 +0700

    Show data from database

--- mywebsite/webservice.php
+++ mywebsite/webservice.php
@@ -3,12 +3,38 @@
 $servername = 'localhost';
 $username ='root';
 $password = 'root';
+$database = 'leappskillFlutter';
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 if($conn -> connect_error){
   echo json_encode(die('Connection failed: ' . $conn->connect_error));
 }
-echo 'Connection Successfully!';
+
+$sql = 'SELECT * FROM testing';
+$result = mysqli_query($conn, $sql);
+
+$myData = array();
+
+if($result -> num_rows > 0){
+  while($row = mysqli_fetch_assoc($result)){
+    $myData[] = $row; 
+
+    $output = array(
+      'status' => 'successfully!',
+      'data' => $myData
+    );
+  }
+}else{
+
+  $output = array(
+    'status' => 'failed!',
+    'data'=> 'No Record Found!'
+  );
+}
+
+echo json_encode($output);
+
+// echo 'Connection Successfully!';
 $conn->close()
 
 ?>
